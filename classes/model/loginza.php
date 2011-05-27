b0282afbc77f281cfd44df534302339e957fbaad
Vitaly <orloff.v@gmail.com>
2011-05-27 17:07:54 +0400

    add signup method

--- classes/model/loginza.php
+++ classes/model/loginza.php
@@ -38,6 +38,38 @@ class Model_Loginza extends Jelly_Model {
     {
         return ! A1::instance()->login_loginza($system['provider'], $system['identity'], $remember) === FALSE;
     }
+    
+    public static function signup($data)
+    {
+        $data['is_loginza'] = TRUE;
+        
+        $loginza_data = Session::instance()->get('loginza_data');
+
+        if ( ! isset($loginza_data['profile']['email']) OR $data['email'] != $loginza_data['profile']['email'])
+        {
+           $data['active'] = FALSE;
+        }
+        else
+        {
+            $data['active'] = TRUE;
+        }
+
+        $member = Jelly::factory($this->module_name, $data)->save();
+
+        if ($member->saved())
+        {
+            Session::instance()->delete('loginza_data');
+
+            Model_Loginza::add_provider($member->id, $loginza_data['system']);
+
+            if ( ! $_POST['active'])
+            {
+                self::emailactivate($member->id, $member->email);
+            }
+        }
+        
+        return $member;
+    }
 
 }
   
