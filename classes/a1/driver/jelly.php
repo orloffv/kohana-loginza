90fab0f7f44a4282826d8a3979e42b1070a8cc18
Vitaly <orloff.v@gmail.com>
2011-05-27 11:23:50 +0400

    fix A1

--- classes/a1/driver/jelly.php
+++ classes/a1/driver/jelly.php
@@ -2,38 +2,11 @@
 
 abstract class A1_Driver_Jelly extends A1 {
 
-	/**
-	 * Loads the user object from database using the token (restored from cookie)
-	 *
-	 * @param   array   token (token and ID)
-	 * @return  object  User Object
-	 */
-	protected function _load_user_by_token(array $token)
-	{
-        return Jelly::select($this->_config['user_model'])
-                ->where($this->_config['columns']['token'], '=', $token[0])->where('id', '=', $token[1])->limit(1)->execute();
+	protected function _load_user($username)
+    {
+        return parent::_load_user($username);
     }
 
-	/**
-	 * Loads the user object from database using username
-	 *
-	 * @param   string   username
-	 * @return  object   User Object
-	 */
-	protected function _load_user($username)
-	{
-        $query = Jelly::select($this->_config['user_model'])->where($this->_config['columns']['username'], '=', $username);
-
-        if (isset($this->_config['columns']['active']))
-        {
-            $query = $query->where($this->_config['columns']['active'], '=', TRUE);
-        }
-
-        $query = $query->limit(1)->execute();
-        
-		return $query;
-	}
-    
     protected function _load_user_loginza($provider, $identity)
 	{
         $query = Jelly::select($this->_config['user_model'])
@@ -49,4 +22,19 @@ abstract class A1_Driver_Jelly extends A1 {
         
 		return $query;
 	}
+    
+    public function login_loginza($provider, $identity, $remember = FALSE)
+    {
+        if (empty($provider) OR empty($identity))
+        {
+            return FALSE;
+        }
+        
+        $user = $this->_load_user_loginza($provider, $identity);
+        
+        if ( $user->loaded())
+		{
+            return $this->complete_login($user,$remember);
+		}
+    }
 }
