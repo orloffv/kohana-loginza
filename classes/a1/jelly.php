2cb6feb9f0f71deabace77858744a794937d6a10
Vitaly <orloff.v@gmail.com>
2011-05-30 16:37:17 +0400

    очередной фикс A1

--- classes/a1/jelly.php
+++ classes/a1/jelly.php
@@ -1,3 +1,39 @@
 <?php defined('SYSPATH') or die('No direct script access.'); 
 
-class A1_Jelly extends A1_Driver_Jelly {}
+class A1_Jelly extends A1_Driver_Jelly {
+    
+    protected function _load_user($username)
+    {
+        return parent::_load_user($username);
+    }
+
+    protected function _load_user_loginza($provider, $identity)
+	{
+        $query = Jelly::select($this->_config['user_model'])
+                ->join('loginza')->on('loginza.'.$this->_config['user_model'].'_id', '=', $this->_config['user_model'].'.id')
+                ->where('loginza.provider', '=', $provider)->where('loginza.identity', '=', $identity);
+
+        if (isset($this->_config['columns']['active']))
+        {
+            $query = $query->where($this->_config['columns']['active'], '=', TRUE);
+        }
+
+        return $query->limit(1)->execute();
+	}
+    
+    public function login_loginza($provider, $identity, $remember = FALSE)
+    {
+        if (empty($provider) OR empty($identity))
+        {
+            return FALSE;
+        }
+        
+        $user = $this->_load_user_loginza($provider, $identity);
+        
+        if ( $user->loaded())
+		{
+            return $this->complete_login($user,$remember);
+		}
+    }
+    
+}
